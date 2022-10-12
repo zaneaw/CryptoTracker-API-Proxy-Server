63740749fa7bbfaf338224d025fd50b7988b3202
Zane Wilson <wilsonzanea@gmail.com>
2022-10-12 19:36:42 -0400

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Rate Limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 Mins
+    max: 500
+});
+
+// Set static folder
+app.use(express.static('public'));
+
+app.use(limiter);
+app.set('trust proxy', 1)
+
+// Routes
+app.use('/api', require('./routes/index.js'))
+
+// Enable cors
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
---
+++ routes/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+// const apicache = require('apicache');
+
+
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+const API_HOST_NAME = process.env.API_HOST_NAME;
+
+// Init cache
+// let cache = apicache.middleware;
+
+router.get('/*', async (req, res) => {
+    try {
+        const endpoint = req.query.endpoint ? req.query.endpoint : 'markets';
+
+        console.log(endpoint)
+
+        const options = {
+            url: `${API_BASE_URL}/coins/${endpoint ? endpoint : ''}`,
+            method: 'GET',
+            headers: {
+                "X-RapidAPI-Key": "86b2a5aaf0mshcf4f7bee0bc4934p12d2cfjsnafa1cf1a7a75",
+                "X-RapidAPI-Host": API_HOST_NAME,
+            },
+            params: {
+                vs_currency: 'usd',
+                page: 1,
+                per_page: 100,
+                order: 'market_cap_desc',
+            },
+        }
+
+        // console.log(url.parse(req.url, true).query)
+        // const params = new URLSearchParams({
+        //     ...url.parse(req.url, true).query
+        // })
+
+        if (process.env.NODE_ENV !== 'production') {
+            console.log(`REQUEST: ${options.url}`)
+        }
+
+        const data = await axios.request(options)
+            .then(res => {
+                const resData = res.data;
+                // console.log(resData);
+                // res.status(200).json(data);
+                return resData;
+            }).catch(error => {
+                // console.error(error);
+                // res.status(500).json({error})
+                return error;
+        });
+
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(500).json({ error });
+    }
+});
+
+module.exports = router;
