7c55da6f4877a9273fcfddada23b43d383c330c3
Zane Wilson <wilsonzanea@gmail.com>
2022-11-22 23:08:29 -0500

    allow origin

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 const API_HOST_NAME = process.env.API_HOST_NAME;
+const ALLOW_ORIGIN = process.env.ALLOW_ORIGIN;
 
 // Init cache
 // let cache = apicache.middleware;
@@ -23,6 +24,7 @@ router.get('/*', async (req, res) => {
             headers: {
                 "X-RapidAPI-Key": API_KEY_VALUE,
                 "X-RapidAPI-Host": API_HOST_NAME,
+                "Access-Control-Allow-Origin": ALLOW_ORIGIN
             },
             params: {
                 vs_currency: 'usd',
