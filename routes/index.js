3f596a95b6455ad58d4152754014e8b7c4d3a6bf
Zane Wilson <wilsonzanea@gmail.com>
2023-01-30 15:47:01 -0500

    formatting, single coin ohlc

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/get-all-coins', async (req, res) => {
         headers: {
             // "X-RapidAPI-Key": API_KEY_VALUE,
             // "X-RapidAPI-Host": API_HOST_NAME,
-            "Access-Control-Allow-Origin": ALLOW_ORIGIN
+            'Access-Control-Allow-Origin': ALLOW_ORIGIN,
         },
         params: {
             vs_currency: 'usd',
@@ -22,17 +22,20 @@ router.get('/get-all-coins', async (req, res) => {
             per_page: 250,
             order: 'market_cap_desc',
         },
-    }
+    };
 
-    await axios.request(options)
-        .then(response => {
+    await axios
+        .request(options)
+        .then((response) => {
             const responseData = response.data;
             return res.status(200).json(responseData);
-        }).catch(error => {
-            return res.status(error.response.status).json(error.response.statusText);
-    });
+        })
+        .catch((error) => {
+            return res
+                .status(error.response.status)
+                .json(error.response.statusText);
+        });
 });
-
 
 router.get('/get-single-coin/:coinId', async (req, res) => {
     const coinId = req.params.coinId;
@@ -44,7 +47,7 @@ router.get('/get-single-coin/:coinId', async (req, res) => {
         headers: {
             // "X-RapidAPI-Key": API_KEY_VALUE,
             // "X-RapidAPI-Host": API_HOST_NAME,
-            "Access-Control-Allow-Origin": ALLOW_ORIGIN
+            'Access-Control-Allow-Origin': ALLOW_ORIGIN,
         },
         params: {
             localization: false,
@@ -52,15 +55,60 @@ router.get('/get-single-coin/:coinId', async (req, res) => {
             developer_data: false,
             sparkline: true,
         },
-    }
+    };
 
-    await axios.request(options)
-        .then(response => {
+    await axios
+        .request(options)
+        .then((response) => {
             const responseData = response.data;
             return res.status(200).json(responseData);
-        }).catch(error => {
-            return res.status(error.response.status).json(error.response.statusText);
+        })
+        .catch((error) => {
+            return res
+                .status(error.response.status)
+                .json(error.response.statusText);
         });
-})
+});
+
+router.get('/get-single-coin-ohlc/:coinId', async (req, res) => {
+    const coinId = req.params.coinId;
+    const amountOfDays = req.query.days ? req.query.days : 1;
+    const currency = req.query.currency ? req.query.currency : 'usd';
+
+    // let finalData = [];
+
+    const options = {
+        url: `${API_BASE_URL}/coins/${coinId}/ohlc`,
+        method: 'GET',
+        cache: 'no-cache',
+        headers: {
+            // "X-RapidAPI-Key": API_KEY_VALUE,
+            // "X-RapidAPI-Host": API_HOST_NAME,
+            'Access-Control-Allow-Origin': ALLOW_ORIGIN,
+        },
+        params: {
+            vs_currency: currency,
+            days: amountOfDays,
+        },
+    };
+
+    const ohlcData = await axios
+        .request(options)
+        .then((response) => {
+            const responseData = response.data;
+            return res.status(200).json(responseData);
+        })
+        .catch((error) => {
+            return res
+                .status(error.response.status)
+                .json(error.response.statusText);
+        });
+
+    // ohlcData.forEach((timeframe, i) => {
+    //     finalData
+    // })
+
+    // return ohlcData[0];
+});
 
 module.exports = router;
